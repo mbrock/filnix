92ee182b781c45088b293f81d7417be02ebbc132
Mikael Brockman <mikael@brockman.se>
2025-12-03 17:43:49 +0200

    more ruby extension porting

--- rubyports.nix
+++ rubyports.nix
@@ -6,14 +6,28 @@ let
     for
     use
     ;
+
+  replace = file: old: new:
+    use (attrs: {
+      postPatch = (attrs.postPatch or "") + ''
+        substituteInPlace ${file} \
+          --replace-fail ${pkgs.lib.escapeShellArg old} \
+                         ${pkgs.lib.escapeShellArg new}
+      '';
+    });
+
 in
 [
   (for "ruby-terminfo" [
-    (use (attrs: {
-      postPatch = (attrs.postPatch or "") + ''
-        substituteInPlace terminfo.c \
-          --replace 'RTEST(ret)' 'RTEST((VALUE)ret)'
-      '';
-    }))
+    (replace "terminfo.c" "RTEST(ret)" "RTEST((VALUE)ret)")
+  ])
+
+  (for "msgpack" [
+    (replace "ext/msgpack/buffer_class.c"
+      "unsigned long max = ((VALUE*) args)[2];"
+      "unsigned long max = (unsigned long)(uintptr_t)((VALUE*) args)[2];")
+    (replace "ext/msgpack/buffer_class.c"
+      "(VALUE) max,"
+      "(VALUE)(uintptr_t) max,")
   ])
 ]
